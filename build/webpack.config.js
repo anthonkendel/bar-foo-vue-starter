fd94aafa1b237e4f940ed1cc509f64d54a3443e6
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-06-27 20:54:00 +0200

    Remove publichPath from webpack config
    Remove publicPath as this is not needed for this template. For more
    advance use it would be beneficial.

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   entry: 'src/main.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/',
     filename: 'build.js',
   },
   mode: isProduction ? 'production' : 'development',
